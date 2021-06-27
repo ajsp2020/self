b80e87de1cd9dbc83869d6f3233967409cd309ed
ajsp2020 <ajpereira88@gmail.com>
2021-06-27 12:37:36 -0300

    27/06/2021

---
+++ projetos/imoveis/dataImovies.py
@@ -0,0 +1,123 @@
+from urllib.request import urlopen, Request
+from urllib.error import URLError, HTTPError
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def site(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+
+    try:
+        req = Request(url, headers=headers)
+        response = urlopen(req)
+        html = response.read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
+
+    except HTTPError as e:
+        print(e.status, e.reason)
+
+    except URLError as e:
+        print(e.reason)
+
+
+def dfImoveis():
+    url = 'https://www.dfimoveis.com.br/venda/df/todos/imoveis?pagina=1'
+    soup = site(url)
+
+    cards = []
+    for i in range(50):
+        # Obtendo o HTML
+        url = 'https://www.dfimoveis.com.br/venda/df/todos/imoveis?pagina=' + str(i + 1)
+        soup = site(url)
+
+        anuncios = soup.find('div', {"id": "resultadoDaBuscaDeImoveis"}).findAll('div', class_="property__info")
+
+        for anuncio in anuncios:
+            card = {}
+            # Endereços
+            endereco = anuncio.find('h3').find('a').getText().replace('\r\n', '').replace('\n', '')
+            endereco = " ".join(endereco.split())
+            card['address'] = endereco
+
+            # Valor
+            precos = anuncio.find('h4', {"class": "property__subtitle hide-mobile"}).findAll('span', class_="price")
+            i = 0
+            valores = []
+            for preco in precos:
+                valores.append(preco.get_text())
+
+            for i in range(len(valores)):
+                try:
+                    card['price'] = valores[0]
+                    card['price_m2'] = valores[1]
+                except:
+                    card['price'] = valores[0]
+
+            # Opções
+            items = anuncio.find('div', {"class": "property__info-content"}).findAll('li')
+            if items[0].get_text() == items[1].get_text():
+                del items[0]
+
+            opcoes = []
+            for item in items:
+                opcoes.append(item.get_text())
+            card['options'] = opcoes
+
+
+            cards.append(card)
+
+    dataset = pd.DataFrame(cards)
+    dataset.to_csv('./output/data/dataset_dfImoveis.csv', index=False, encoding='utf-8-sig')
+    return dataset
+
+
+def zapImoveisBrasilia(pages):
+
+    cards = []
+    for i in range(pages):
+        url = 'https://www.zapimoveis.com.br/venda/imoveis/df+brasilia/?pagina=' + str(i + 1)
+        soup = site(url)
+        anuncios = soup.find('div', {"class": "listings__container"}).findAll('div', class_="card-listing simple-card js-listing-card")
+
+        for anuncio in anuncios:
+            card = {}
+            # Valor
+            value = anuncio.find('p', {"class": "simple-card__price js-price heading-regular heading-regular__bolder align-left"}).getText()
+            _, value = " ".join(value.split()).split()
+            card['value'] = value
+
+            # Endereço
+            address = anuncio.find('p', {"class": "color-dark text-regular simple-card__address"}).getText()
+            address = " ".join(address.split())
+
+            try:
+                address1, address2 = address.split(',')
+                card['address_1'] = address1
+                card['address_2'] = address2
+            except:
+                card['address_1'] = address
+
+            # feature
+            items = anuncio.find('div', {"class": "simple-card__actions"}).ul.findAll('li')
+
+            for item in items:
+                feature = item.get_text()
+                feature = " ".join(feature.split())
+                try:
+                    feature, _ = feature.split()
+                    card[item.get('class')[2].split('-')[-1]] = feature
+                except:
+                    card[item.get('class')[2].split('-')[-1]] = feature
+
+            cards.append(card)
+
+    dataset = pd.DataFrame(cards)
+    dataset.to_csv('./output/data/zapImoveis.csv', index=False, encoding='utf-8-sig')
+    return dataset
+
+
+
+
+
--- projetos/imoveis/imoveis.py
+++ projetos/imoveis/imoveis.py
@@ -1,117 +1,62 @@
-from urllib.request import urlopen, Request
-from urllib.error import URLError, HTTPError
-
 import pandas as pd
-from bs4 import BeautifulSoup
+from dataImovies import zapImoveisBrasilia
 
-def site(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+def atualiza_dados():
 
-    try:
-        req = Request(url, headers=headers)
-        response = urlopen(req)
-        html = response.read().decode('utf-8')
-        soup = BeautifulSoup(html, 'html.parser')
-        return soup
+    atualiza = input("Deseja atualizar a base de dados? ").capitalize()
 
-    except HTTPError as e:
-        print(e.status, e.reason)
+    if atualiza == 'S':
+        pages = int(input("Quantas paginas deseja? "))
 
-    except URLError as e:
-        print(e.reason)
+        if pages < 4:
+            zapImoveisBrasilia(4)
+        else:
+            zapImoveisBrasilia(pages)
 
+        print("Busca de dados concluída!")
 
-def dfImoveis():
-    url = 'https://www.dfimoveis.com.br/venda/df/todos/imoveis?pagina=1'
-    soup = site(url)
+    elif atualiza_dados == 'N':
+        pass
 
-    cards = []
-    for i in range(50):
-        # Obtendo o HTML
-        url = 'https://www.dfimoveis.com.br/venda/df/todos/imoveis?pagina=' + str(i + 1)
-        soup = site(url)
-
-        anuncios = soup.find('div', {"id": "resultadoDaBuscaDeImoveis"}).findAll('div', class_="property__info")
-
-        for anuncio in anuncios:
-            card = {}
-            # Endereços
-            endereco = anuncio.find('h3').find('a').getText().replace('\r\n', '').replace('\n', '')
-            endereco = " ".join(endereco.split())
-            card['address'] = endereco
-
-            # Valor
-            precos = anuncio.find('h4', {"class": "property__subtitle hide-mobile"}).findAll('span', class_="price")
-            i = 0
-            valores = []
-            for preco in precos:
-                valores.append(preco.get_text())
-
-            for i in range(len(valores)):
-                try:
-                    card['price'] = valores[0]
-                    card['price_m2'] = valores[1]
-                except:
-                    card['price'] = valores[0]
-
-            # Opções
-            items = anuncio.find('div', {"class": "property__info-content"}).findAll('li')
-            if items[0].get_text() == items[1].get_text():
-                del items[0]
-
-            opcoes = []
-            for item in items:
-                opcoes.append(item.get_text())
-            card['options'] = opcoes
+    else:
+        raise ValueError("Valor de entrada incorreto!")
 
 
-            cards.append(card)
+def trata_dados():
 
-    dataset = pd.DataFrame(cards)
-    dataset.to_csv('./output/data/dataset_dfImoveis.csv', index=False, encoding='utf-8-sig')
-    return dataset
+    df = pd.read_csv('./output/data/zapImoveis.csv')
+    df = df.drop(columns=['address_1'])
+    df = df.dropna().reset_index(drop=True)
 
+    for i in range(len(df)):
+        try:
+            df.loc[i, "value"] = float(df.loc[i, "value"].replace('.', ''))
+            try:
+                df.loc[i, "areas", "bedrooms", "spaces", "bathrooms"] = int(df.loc[i, "areas", "bedrooms", "spaces",
+                                                                                   "bathrooms"])
 
-def zapImoveisBrasilia():
-    # url = 'https://www.zapimoveis.com.br/venda/imoveis/df+brasilia/?pagina=1'
-    # soup = site(url)
-    pages = 100
+            except:
+                df.loc[i, "areas"] = int(df.loc[i, "areas"].split(sep=" ")[0])
+                df.loc[i, "bedrooms"] = int(df.loc[i, "bedrooms"].split(sep=" ")[0])
+                df.loc[i, "spaces"] = int(df.loc[i, "spaces"].split(sep=" ")[0])
+                df.loc[i, "bathrooms"] = int(df.loc[i, "bathrooms"].split(sep=" ")[0])
 
-    cards = []
-    for i in range(pages):
-        url = 'https://www.zapimoveis.com.br/venda/imoveis/df+brasilia/?pagina=' + str(i + 1)
-        soup = site(url)
-        anuncios = soup.find('div', {"class": "listings__container"}).findAll('div', class_="card-listing simple-card js-listing-card")
+        except:
+            df = df.drop(labels=i, axis=0)
 
-        for anuncio in anuncios:
-            card = {}
-            # Valor
-            value = anuncio.find('p', {"class": "simple-card__price js-price heading-regular heading-regular__bolder align-left"}).getText()
-            value = " ".join(value.split())
-            card['value'] = value
+    df['m2'] = (df.value / df.areas).apply(lambda x: round(x))
 
-            # Endereço
-            address = anuncio.find('p', {"class": "color-dark text-regular simple-card__address"}).getText()
-            address = " ".join(address.split())
-            card['address'] = address
+    a_renomar = {'value': 'preco',
+                 'address_2': 'bairro',
+                 'areas': 'area_total',
+                 'bedrooms': 'quartos',
+                 'spaces': 'vagas',
+                 'bathrooms': 'banheiros'}
 
-            # feature
-            items = anuncio.find('div', {"class": "simple-card__actions"}).ul.findAll('li')
+    df = df.rename(columns=a_renomar)
 
-            for item in items:
-                feature = item.get_text()
-                feature = " ".join(feature.split())
-                card[item.get('class')[2].split('-')[-1]] = feature
-
-            cards.append(card)
-
-    dataset = pd.DataFrame(cards)
-    dataset.to_csv('./output/data/zapImoveis.csv', index=False, encoding='utf-8-sig')
-    return dataset
+    print(df)
 
 
 
-
-
-print(zapImoveisBrasilia())
+trata_dados()
